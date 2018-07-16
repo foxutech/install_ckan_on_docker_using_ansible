d2d8725277d5c0560ea3d9fe8fff8e0023e6085e
FoxuTech <prabulugar@gmail.com>
2018-07-16 18:58:16 +0530

    Update ansibleinstall.sh

--- ansibleinstall.sh
+++ ansibleinstall.sh
@@ -4,4 +4,9 @@ apt-add-repository ppa:ansible/ansible
 echo "update the packages"
 apt update
 echo "Install Ansible"
-apt install ansible
+apt install ansible python-pip -y
+echo "install boto tools"
+/usr/bin/python -m easy_install pip
+/usr/bin/python -m pip install boto
+/usr/bin/python -m pip install boto3
+/usr/bin/python -m pip install docker-compose
